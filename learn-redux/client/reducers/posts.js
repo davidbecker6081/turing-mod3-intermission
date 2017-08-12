2b38f234988174a3a64c0ec4a5c945eacd1f4aaf
David Becker <david@beckersite.com>
2017-08-12 16:34:48 -0600

    Add dispatch and map to props

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,7 @@
 //a reducer takes two things: the action and copy of current state
 
 function posts(state = [], action) {
+  console.log('post will change');
   console.log(state, action);
   return state
 }
